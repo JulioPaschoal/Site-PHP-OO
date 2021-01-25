291d35f5724136d0cdf907b0bc917f253f0817c4
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2021-01-25 20:35:13 -0300

    listando Serviços

--- app/sts/Controllers/Home.php
+++ app/sts/Controllers/Home.php
@@ -18,6 +18,9 @@ class Home
         $listar_car = new \Sts\Models\StsCarousel();
         $this->Dados['sts_carousels'] = $listar_car->listar();
 
+        $listar_ser = new \Sts\Models\StsServico();
+        $this->Dados['sts_servicos'] = $listar_ser->listar();
+
         $carregarView = new \Core\ConfigView("sts/Views/home/home", $this->Dados);
         $carregarView->renderizar();
     }
--- app/sts/Views/home/home.php
+++ app/sts/Views/home/home.php
@@ -28,12 +28,16 @@ if (!defined('URL')) {
                     echo 'active';
                 }
                 ?>">
-                    <img class="first-slide img-fluid" src="<?php echo URL . 'assets/imagens/carousel/' . $id . '/' . $imagem; ?>" alt="<?php echo $titulo; ?>">
+                    <img class="first-slide img-fluid"
+                         src="<?php echo URL . 'assets/imagens/carousel/' . $id . '/' . $imagem; ?>"
+                         alt="<?php echo $titulo; ?>">
                     <div class="container">
                         <div class="carousel-caption <?php echo $posicao_text; ?>">
                             <h1 class="d-none d-md-block"><?php echo $titulo; ?></h1>
                             <p class="d-none d-md-block"><?php echo $descricao; ?></p>
-                            <p class="d-none d-md-block"><a class="btn btn-lg btn-<?php echo $cor; ?>" href="<?php echo $link; ?>" role="button"><?php echo $titulo_botao; ?></a></p>
+                            <p class="d-none d-md-block"><a class="btn btn-lg btn-<?php echo $cor; ?>"
+                                                            href="<?php echo $link; ?>"
+                                                            role="button"><?php echo $titulo_botao; ?></a></p>
                         </div>
                     </div>
                 </div>
@@ -50,5 +54,51 @@ if (!defined('URL')) {
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
+
+        <?php extract($this->Dados['sts_servicos'][0]); ?>
+        <div class="jumbotron servicos">
+            <div class="container">
+                <h2 class="display-4 text-center" style="margin-bottom: 40px;"><?php echo $titulo; ?></h2>
+                <div class="card-deck">
+                    <div class="card text-center card-um">
+                        <div class="icon-row tamanh-icone">
+								<span class="step size-96 text-danger">
+									<i class="icon ion-ios-camera-outline"></i>
+								</span>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Serviço um</h5>
+                            <p class="card-text lead">This is a longer card with supporting text below as a natural
+                                lead-in to additional content. This content is a little bit longer.</p>
+                        </div>
+                    </div>
+                    <div class="card text-center card-dois">
+                        <div class="icon-row tamanh-icone">
+								<span class="step size-96 text-danger">
+									<i class="icon ion-ios-film-outline"></i>
+								</span>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Serviços dois</h5>
+                            <p class="card-text lead">This card has supporting text below as a natural lead-in to
+                                additional content.</p>
+                        </div>
+                    </div>
+                    <div class="card text-center card-tres">
+                        <div class="icon-row tamanh-icone ">
+								<span class="step size-96 text-danger">
+									<i class="icon ion-ios-videocam-outline"></i>
+								</span>
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Serviço tres</h5>
+                            <p class="card-text lead">This is a wider card with supporting text below as a natural
+                                lead-in to additional content. This card has even longer content than the first to show
+                                that equal height action.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </main>
